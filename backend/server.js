23a74f41b63664abea74a3a148879ee1cc68fd6e
Jeremy Lau <aus.jeremy.lau@gmail.com>
2024-07-08 00:05:42 +1000

    further reconfigs and changes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const cors = require("cors");
+const https = require("https");
+const getAutomotiveMechanicsPrompt = require("./prompt_types/automotive_mechanics")
+const getDomesticCleaningServicesPrompt = require("./prompt_types/domestic_cleaning_services")
+const getTelephonyAIServiceEnquiryPrompt = require("./prompt_types/telephony_ai_service_enquiry")
 
 // Server setup
 app.use(cors());
@@ -18,86 +22,46 @@ app.post("/request-demo", (req, res) => {
   console.log("Received data:", req.body);
 
   // Parse the form values
-  const { name, phoneNumber, companyName, role, useCase } = req.body;
+  const { name, phoneNumber, companyName, businessType, useCase } = req.body;
+
+  let prompt;
+  if (businessType === "Automotive Mechanics") {
+    prompt = getAutomotiveMechanicsPrompt(name, companyName, businessType, useCase);
+  } else if (businessType === "Cleaning Services") {
+    prompt = getDomesticCleaningServicesPrompt(name, companyName, businessType, useCase);
+  } else {
+    prompt = getTelephonyAIServiceEnquiryPrompt(name, companyName, businessType, useCase);
+  };
+
+  let newTransferPhoneNumber = "+61400878456"; // my secondary number as default Chirp AI customer support number
+  let voiceId = "3a1e0243-ef27-4420-9b53-89f343eb49d4" // This is a private female aussie voice
+  let interruptionThreshold = 175
 
   // Set the prompt for the AI. Insert the form values directly into the prompt.
-  const prompt = `BACKGROUND INFO: 
-  Your name is Jonathan and you're part of the GTM (Go to market) team at BlandAI. Your job is to call and qualify inbound leads right after they submit an inquiry on the BlandAI website. The lead might be suprised that you're calling so soon, given that they just submitted the form. That's okay. If asked, explain that you are an AI phone agent, and that your job is to provdide support to Bland AI customers.
-
-  Greeting the Lead
-
-  Answer all inbound calls within 5 minutes of form submission
-  Greet the lead in a friendly, upbeat tone
-  Introduce yourself by first name and company
-  Confirm you are speaking with the lead by referencing the form they filled out
-  Thank them for taking the time to reach out to Bland AI
-  Qualifying the Lead
-
-  Ask open-ended questions to understand their use case and needs:
-  What challenges are you looking to solve with our platform?
-  How do you envision using our AI capabilities?
-  What is the scale of your potential implementation?
-  Listen closely to gauge the quality and viability of the use case
-  If details are vague or use case seems small-scale, follow email outreach instructions
-  If use case seems high-quality with sizable volume, follow call transfer instructions
-  Follow Up Over Email
-
-  If use case appears flimsy or low volume:
-
-  Maintain a warm, helpful tone
-  Say you’d be happy to follow up over email to provide more information
-  Offer to send product specs, pricing, and a custom demo if helpful
-  Thank them again for reaching out and confirm you’ll follow up
-
-  If use case seems high quality with sizable volume:
-
-  Enthusiastically say you have the perfect team member to discuss further
-  Confirm you can transfer them directly to move the discussion forward
-  Thank them for their time and introduce them quickly to new team member
-  Politely wrap up and transfer the call
-
-  EXAMPLE DIALOGUE:
-  You: Hey ${name}
-  Them: Hi who's this?
-  You: This is Jonathan from the GTM team at Bland AI. You just submitted an inquiry?
-  Them: Oh hey Jonathan, yeah I did, thanks for following up so soon.
-  You: Of course. Could you tell me what prompted you to reach out?
-  Them: Definitley. We want to send phone calls to our e-commerce leads. Both to collect feedback and also to offer them promotions for repeat purchases.
-  You: That's awesome, I love that use case. How many of these phone calls are you looking to send?
-  Them: Probably a few hundred per week to start. And then later, I'd love to send one to every single customer; probably tens of thousands a month.
-  You: Okay, perfect. I'd love to connect you with one of my colleagues to offer further support. Could I go ahead and transfer you?
-  Them: Yeah that sounds great, go for it.
-  You: Okay! Great meeting you ${name}, I'll go ahead and transfer you now
-  USES TRANSFER TOOL
-  
-  INFORMATION ABOUT YOUR PROSPECT:
-  * Their name is ${name}
-  * Their company's name is ${companyName}
-  * Their role is ${role}
-  * Their use case is ${useCase}
-  `;
-
-  // After the phone agent qualifies the lead, they'll transfer to this phone number
-  const TRANSFER_PHONE_NUMBER = "XXX-XXX-XXXX";
-
-  // Create the parameters for the phone call. Ref: https://docs.bland.ai/api-reference/endpoint/call
   const data = {
     phone_number: phoneNumber,
     task: prompt,
-    voice_id: 1,
     reduce_latency: false,
-    transfer_phone_number: TRANSFER_PHONE_NUMBER,
+    transfer_phone_number: newTransferPhoneNumber,
+    voice: voiceId,
+    interruption_threshold: interruptionThreshold,
   };
 
+
+  // create a http agent with the option to reject SSL cert validation due to issue i am encountering. 
+  const httpAgent = new https.Agent({ rejectUnauthorized: false });
+  
   // Dispatch the phone call
   axios
-    .post("https://api.bland.ai/call", data, {
+    .post("https://api.bland.ai/v1/calls", data, {
       headers: {
         authorization: apiKey,
         "Content-Type": "application/json",
       },
+      httpsAgent: httpAgent,
     })
     .then((response) => {
+      console.log("Response:", response.data);
       const { status } = response.data;
 
       if (status) {
