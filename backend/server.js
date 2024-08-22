ace67d238a4e83003059acf0cd14ccb347eed3b2
Jeremy Lau <aus.jeremy.lau@gmail.com>
2024-08-22 23:41:56 +1000

    Setup updateContext API for agent

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,45 @@ app.use(express.json());
 const addressChangeEndPoint = process.env.ADDRESS_UPDATE_ZAP_ENDPOINT;
 const apiKey = process.env.BLAND_API_KEY;
 const PORT = process.env.PORT || 4000;
+  
+// Setup an Ngrok Web Address to forward webhooks to local port. It also automatically updates zapier with new transient address
+ngrok.connect({ addr: 4000, authtoken_from_env: true })
+    .then(listener => {console.log('Ngrok transient web address running on:', listener);
 
+    const newAddressData = [{
+      "newTransientWebAddress": listener,
+      "webHookEndPoint": webhookEndpoint
+    }];
+    // Update this zap webhook address if you choose to point to a different account. 
+    const zapHook = addressChangeEndPoint
+
+    return axios.post(zapHook, newAddressData)
+      .then(response => {
+        console.log('New server instance - webhook address update response:', response.data);
+      })
+      .catch(err => {console.error('Webhook address update error message:', err);
+      });
+    })
+
+    .catch(err => console.error('Ngrok error message:', err));
+
+    
+
+// Set in memory storage for the session
+const objStorageArray = [];
+const cachedObjData = [];
 
 // Handle form submissions
 app.post("/request-demo", (req, res) => {
   // Data succesfully received from Frontend
   console.log("Received data:", req.body);
+
+  // Setup session data identifier and objects
+  let storageObjId = Math.random().toString(36).substring(2, 10);
+  let storageObj = [{context_id: storageObjId}];
+  objStorageArray.push(storageObj);
+  console.log("Storage object ID:", objStorageArray);
+
 
   // Parse the form values
   const { name, phoneNumber, companyName, businessType, useCase } = req.body;
@@ -39,6 +72,9 @@ app.post("/request-demo", (req, res) => {
     prompt = getTelephonyAIServiceEnquiryPrompt(name, companyName, businessType, useCase);
   };
 
+
+
+
   let newTransferPhoneNumber = "+61400878456"; // my secondary number as default Chirp AI customer support number
   let voiceId = "88831b36-7c85-4879-b6b0-22c2ff9f59d7" // This is a bland.ai public Australian Female voice called Lucy.
   let interruptionThreshold = 175
@@ -49,6 +85,7 @@ app.post("/request-demo", (req, res) => {
     phone_number: phoneNumber,
     task: prompt,
     tools: [updateBookingTool, bookAppointmentTool],
+    dynamic_data: updateContext,
     reduce_latency: true,
     language: "en",
     voice: voiceId,
@@ -100,7 +137,7 @@ const webhookEndpoint = "/agentchirplistener";
 app.post(webhookEndpoint, (req, res) => {
   console.log("Received data:", req.body);
 
-  // process webhook data here
+  // process webhook data here into session storage
   console.log(req.body);
 
   res.status(200).send({ message: "Webhook received", status: "success"});
@@ -109,25 +146,46 @@ app.post(webhookEndpoint, (req, res) => {
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-// Setup an Ngrok Web Address to forward webhooks to local port. It also automatically updates zapier with new transient address
-ngrok.connect({ addr: 4000, authtoken_from_env: true })
-    .then(listener => {console.log('Ngrok transient web address running on:', listener);
+// API GET endpoint for dynamically passing data to blandAI agent during call.
+app.get("/update-context", (req, res) => {
+  let qsp = req.query.id;
 
-    const newAddressData = [{
-      "newTransientWebAddress": listener,
-      "webHookEndPoint": webhookEndpoint
-    }];
-    // Update this zap webhook address if you choose to point to a different account. 
-    const zapHook = addressChangeEndPoint
+  let responseData = {
+    newElements: [],
+    newData: false
+  };
 
-    return axios.post(zapHook, newAddressData)
-      .then(response => {
-        console.log('New server instance - webhook address update response:', response.data);
-      })
-      .catch(err => {console.error('Webhook address update error message:', err);
-      });
-    })
+  const noData = undefined;
 
-    .catch(err => console.error('Ngrok error message:', err));
+// Prepare and send zap data from session storage array. 
+  if (objStorageArray.length > 0 ) {
 
-    
+  let newContext = objStorageArray.find(array => array[0].context_id === qsp);
+  let cachedIndex = cachedObjData.findIndex(array => array.length && array[0].context_id === qsp);
+  let previousContext = cachedIndex !== -1 ? cachedObjData[cachedIndex] : [];
+
+  if (newContext !== undefined) {
+  let newData = newContext.length !== previousContext.length;
+
+  if (newData) {
+    responseData.newElements = newContext.slice(previousContext.length);
+
+    if (cachedIndex !== -1) {
+      cachedObjData.splice(cachedIndex, 1);
+    }
+    cachedObjData.push(newContext);
+  }
+    else { res.status(200).send(noData);
+    }  
+  } else { res.status(200).send(noData);
+    }
+
+  if (newContext !== undefined) {
+  res.status(200).send(responseData.newElements);
+    };
+
+  } else { res.status(200).send(noData);
+    }
+
+  });
+
